9f6ba7de73c1b5fec53bdf6034c736384caea05a
chojs23 <chojs990222@gmail.com>
2021-12-16 14:18:28 +0900

    20211216

--- 1.py
+++ 1.py
@@ -1,38 +1,18 @@
-def solution(new_id):
-    answer = ''
-    temp = new_id.lower()
+def solution(n, lost, reserve):
+    reserve.sort(reverse=True)
+    for i in reserve:
+        if i - 1 == lost[-1] or i + 1 == lost[-1]:
+            lost.pop()
 
-    for x in temp:
-        if x == "-" or x == "_" or x == "." or x.isalpha() or x.isdigit():
-            answer += x
-
-    for i, c in enumerate(answer):
-        print(i, c)
-        if i < len(answer):
-            while answer[i] == "." and answer[i+1] == ".":
-                answer = answer[:i]+answer[i+1:]
-    answer.find
-    if answer[0] == "." and len(answer) == 1:
-        answer = ""
-    elif answer[0] == ".":
-        answer = answer[1:]
-    elif answer[-1] == ".":
-        answer = answer[:-1]
-
-    if not answer:
-        answer += "a"
-    else:
-        answer = answer[:15]
-
-    if len(answer) > 15:
-        answer = answer[:16]
-        if answer[-1] == ".":
-            answer = answer[:-1]
-    if len(answer) < 3:
-        while len(answer) > 2:
-            answer += answer[-1]
-
-    return answer
+        if len(lost) == 0:
+            break
+    answer = 0
+    srt = {}
+    srt.setdefault()
+    print(srt)
+    s = []
+    s.sort()
+    sorted()
 
 
-print(solution(	"=.="))
+print(solution(9, [5, 6, 8, 1, 2], [2, 3, 1, 4, 8, 9]))

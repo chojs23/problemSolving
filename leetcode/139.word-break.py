50866c8f5c8052365d1424e0d506c560be5f395b
chojs23 <chojs990222@gmail.com>
2022-02-17 16:15:03 +0900

    20220217

---
+++ leetcode/139.word-break.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=139 lang=python3
+#
+# [139] Word Break
+#
+
+# @lc code=start
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+# @lc code=end
+

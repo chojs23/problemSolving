245192c85cd900df4240d718f756360a00477139
chojs23 <chojs990222@gmail.com>
2022-02-23 14:11:56 +0900

    20220223

---
+++ leetcode/86.partition-list.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=86 lang=python3
+#
+# [86] Partition List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+# @lc code=end
+

bb6ce75d83709c749e11eae0179459e32e686ff6
chojs23 <chojs990222@gmail.com>
2022-03-21 17:34:04 +0900

    20220321

--- leetcode/23.merge-k-sorted-lists.py
+++ leetcode/23.merge-k-sorted-lists.py
@@ -3,6 +3,7 @@
 #
 # [23] Merge k Sorted Lists
 #
+import heapq
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -11,7 +12,58 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-# @lc code=end
+    def mergeKLists2(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not list or len(lists) == 0:
+            return None
 
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        # TODO
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
+
+    def mergeKLists(self, lists):
+        if not list or len(lists) == 0:
+            return None
+        h = []
+        head = tail = ListNode(0)
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i, lists[i]))
+
+        while h:
+            node = heapq.heappop(h)
+            node = node[2]
+            tail.next = node
+            tail = tail.next
+            if node.next:
+                i += 1
+                heapq.heappush(h, (node.next.val, i, node.next))
+
+        return head.next
+
+
+# @lc code=end

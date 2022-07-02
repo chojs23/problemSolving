a0427b12fe1bd030a2957a911a51c640b3de30e1
chojs23 <chojs990222@gmail.com>
2022-07-02 17:27:53 +0900

    20220702

---
+++ boj/삼성 SW 역량 테스트/17281.py
@@ -0,0 +1,55 @@
+from copy import copy, deepcopy
+from math import sqrt
+import sys
+from collections import defaultdict, deque
+from itertools import combinations, permutations
+import heapq
+import bisect
+
+dr, dc = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+input = sys.stdin.readline
+INF = sys.maxsize
+
+# ===========================================================
+
+n = int(input())
+
+inning = [list(map(int, input().split())) for _ in range(n)]
+
+max_score = 0
+
+
+def game(line_ups):
+    hitter_idx = 0
+    score = 0
+
+    for each_inning in inning:
+        out = 0
+        b1, b2, b3 = 0, 0, 0
+
+        while out < 3:
+            if each_inning[line_ups[hitter_idx]] == 0:
+                out += 1
+            elif each_inning[line_ups[hitter_idx]] == 1:
+                score += b3
+                b1, b2, b3 = 1, b1, b2
+            elif each_inning[line_ups[hitter_idx]] == 2:
+                score += b3 + b2
+                b1, b2, b3 = 0, 1, b1
+            elif each_inning[line_ups[hitter_idx]] == 3:
+                score += b3 + b2 + b1
+                b1, b2, b3 = 0, 0, 1
+            elif each_inning[line_ups[hitter_idx]] == 4:
+                score += b3 + b2 + b1 + 1
+                b1, b2, b3 = 0, 0, 0
+            hitter_idx = (hitter_idx + 1) % 9
+
+    return score
+
+
+for line_ups in permutations(range(1, 9), 8):
+    line_ups = list(line_ups[:3]) + [0] + list(line_ups[3:])
+    max_score = max(max_score, game(line_ups))
+
+print(max_score)

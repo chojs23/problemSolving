3b6fde257b5b824f663769f5b6d2def7f5fb3abe
neo <chojs990222@gmail.com>
2022-07-27 17:40:09 +0900

    20220727

---
+++ boj/스택/2164(queue).py
@@ -0,0 +1,30 @@
+import collections
+import itertools
+from copy import copy, deepcopy
+from math import sqrt
+import sys
+from collections import defaultdict, deque
+from itertools import combinations, permutations
+import heapq
+import bisect
+
+dr, dc = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+input = sys.stdin.readline
+INF = sys.maxsize
+
+# ===========================================================
+
+N = int(input())
+
+q = deque(range(1, N+1))
+
+while True:
+
+    q.popleft()
+    if len(q) == 1:
+        break
+    a = q.popleft()
+    q.append(a)
+
+print(q[0])

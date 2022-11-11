acf0c0e0e971a293f4554f8c0f044fc9f5254953
Neo <chojs990222@gmail.com>
2022-11-11 18:04:43 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,16 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    trucks_on_bridge = [0] * bridge_length
+    bridgeSum=0
+
+    while len(trucks_on_bridge):
+        answer += 1
+        bridgeSum-=trucks_on_bridge.pop(0)
+        if truck_weights:
+            if  bridgeSum + truck_weights[0] <= weight:
+                temp=truck_weights.pop(0)
+                trucks_on_bridge.append(temp)
+                bridgeSum+=temp
+            else:
+                trucks_on_bridge.append(0)
+    return answer

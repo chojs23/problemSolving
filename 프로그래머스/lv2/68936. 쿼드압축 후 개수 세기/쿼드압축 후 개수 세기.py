d79d8fa1e8376ff8971b2277c266a7de2f966c53
Neo <chojs990222@gmail.com>
2022-11-11 19:40:11 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 108.59 ms, Memory: 12.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,22 @@
+def solution(arr):
+    answer = [0,0]
+    length = len(arr)
+
+    def compression(a, b, l):
+        start = arr[a][b]
+
+        for i in range(a, a + l):
+            for j in range(b, b + l):
+                if arr[i][j] != start:
+                    l //= 2
+                    compression(a, b, l)
+                    compression(a, b + l, l)
+                    compression(a + l, b, l)
+                    compression(a + l, b + l, l)
+                    return
+        answer[start]+=1
+    
+
+    compression(0,0,length)
+
+    return answer

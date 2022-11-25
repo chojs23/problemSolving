d2f8adb43585c37bce6e55756a446acfc61a8561
Neo <chojs990222@gmail.com>
2022-11-25 20:51:57 +0900

    [level 3] Title: 기지국 설치, Time: 3.46 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,18 @@
+from math import ceil
+def solution(n, stations, w):
+    answer = 0
+
+    
+    dist = []
+    for i in range(1, len(stations)):
+        dist.append((stations[i]-w-1)-(stations[i-1]+w))
+    
+    dist.append(stations[0]-w-1)
+    dist.append(n-(stations[-1]+w))
+            
+    for t in dist:
+        
+        answer+=ceil(t/(2*w+1))
+    
+    
+    return answer
